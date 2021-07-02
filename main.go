422f3f21526381f49fb29d845f2003dacaf32661
Diego Augusto <daugustomoura@gmail.com>
2021-07-02 15:32:45 -0300

    feature: avoid global template (#20)
    * feature: avoid global templte
    
    * chore: remove .vscode file
    
    * chore: vscode .gitignore section

--- main.go
+++ main.go
@@ -11,35 +11,36 @@ import (
 	"github.com/go-shiori/go-readability"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		keys, ok := r.URL.Query()["q"]
+		if !ok {
+			log.Println("'q' is missing")
+			return
+		}
 
-	keys, ok := r.URL.Query()["q"]
+		q := keys[0]
 
-	if !ok {
-		log.Println("'q' is missing")
-		return
+		article, err := readability.FromURL(q, 30*time.Second)
+		if err != nil {
+			log.Fatalf("failed to parse %s, %v\n", q, err)
+		}
+
+		fmt.Printf("URL     : %s\n", q)
+		fmt.Printf("Title   : %s\n", article.Title)
+		fmt.Printf("Author  : %s\n", article.Byline)
+		fmt.Printf("Length  : %d\n", article.Length)
+		fmt.Printf("Excerpt : %s\n", article.Excerpt)
+		fmt.Printf("SiteName: %s\n", article.SiteName)
+		fmt.Printf("Image   : %s\n", article.Image)
+		fmt.Printf("Favicon : %s\n", article.Favicon)
+		fmt.Println()
+
+		writeResponse(w, tmpl, article)
 	}
-	q := keys[0]
-
-	article, err := readability.FromURL(q, 30*time.Second)
-	if err != nil {
-		log.Fatalf("failed to parse %s, %v\n", q, err)
-	}
-
-	fmt.Printf("URL     : %s\n", q)
-	fmt.Printf("Title   : %s\n", article.Title)
-	fmt.Printf("Author  : %s\n", article.Byline)
-	fmt.Printf("Length  : %d\n", article.Length)
-	fmt.Printf("Excerpt : %s\n", article.Excerpt)
-	fmt.Printf("SiteName: %s\n", article.SiteName)
-	fmt.Printf("Image   : %s\n", article.Image)
-	fmt.Printf("Favicon : %s\n", article.Favicon)
-	fmt.Println()
-
-	writeResponse(w, article)
 }
 
-func writeResponse(w http.ResponseWriter, article readability.Article) {
+func writeResponse(w http.ResponseWriter, tmpl *template.Template, article readability.Article) {
 	if err := tmpl.Execute(w, article); err != nil {
 		log.Println(err)
 	}
@@ -48,21 +49,18 @@ func writeResponse(w http.ResponseWriter, article readability.Article) {
 var (
 	//go:embed template.html
 	html string
-
-	tmpl *template.Template
 )
 
-func parseTemplate() {
-	tmpl = template.New("")
+func parseTemplate() *template.Template {
+	tmpl := template.New("")
 	if _, err := tmpl.Parse(html); err != nil {
 		log.Fatalln(err)
 	}
-
+	return tmpl
 }
 
 func main() {
-	parseTemplate()
-
-	http.HandleFunc("/", handler)
+	tmpl := parseTemplate()
+	http.HandleFunc("/", handler(tmpl))
 	http.ListenAndServe(":8080", nil)
 }
